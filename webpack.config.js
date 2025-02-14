889142b26082c40ce87fc37d9c4512361eb6450b
Frank <wanyoike39@gmail.com>
2025-02-14 15:40:18 +0300

    install another dependency and push the completed webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,41 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.sass$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: '/public/index.html'
+        }),
+    ],
+    devServer: {
+        static: path.resolve(__dirname, './dist'),
+        hot: true,
+        port: 9000,
+        compress: true,
+    }
+}
